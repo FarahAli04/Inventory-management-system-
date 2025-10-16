4fddbe4a3005f029be0329e87b325a9dccd2017a
FarahAli04 <es.farah.ali2023@alexu.edu.eg>
2025-10-16 22:58:49 +0300

    Update EmployeeRole.java

--- lab4/src/main/java/com/example/EmployeeRole.java
+++ lab4/src/main/java/com/example/EmployeeRole.java
@@ -1,5 +1,12 @@
+/*productsDatabase and 
+customerProductDatabase which are objects from ProductDatabase class 
+and CustomerProductDatabase class respectively. 
+There is only one constructor in the class and its header is 
+public EmployeeRole() */
 package com.example;
 
-public class EmployeeRole {
+public class EmployeeRole extends ProductDatabase {
+private ProductDatabase customerProductDatabase = new ProductDatabase();
+
 
 }

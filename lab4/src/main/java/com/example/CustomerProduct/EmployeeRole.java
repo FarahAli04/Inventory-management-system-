7359149cd44de9cca1f5db99fcf67004aac6e814
FarahAli04 <es.farah.ali2023@alexu.edu.eg>
2025-10-17 00:43:25 +0300

    Update EmployeeRole.java

--- lab4/src/main/java/com/example/CustomerProduct/EmployeeRole.java
+++ lab4/src/main/java/com/example/CustomerProduct/EmployeeRole.java
@@ -1,9 +1,3 @@
-/*
-2. public Product[] getListOfProducts(): returns an array that contains all the 
-products stored in the file named Products.txt. 
-3. public CustomerProduct[] getListOfPurchasingOperations(): returns an 
-array that contains all the purchasing operations stored in the file named 
-CustomersProducts.txt */
 package com.example.CustomerProduct;
 
 import java.io.*;

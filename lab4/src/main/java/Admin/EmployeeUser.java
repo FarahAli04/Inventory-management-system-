d96c817cb727f586d9090a52c42062eaf552cd5c
Habiba-Aboghazala9 <es.habiba.salah2023@alexu.edu.eg>
2025-10-18 17:10:51 +0300

    Update EmployeeUser.java

--- lab4/src/main/java/Admin/EmployeeUser.java
+++ lab4/src/main/java/Admin/EmployeeUser.java
@@ -1,7 +1,5 @@
 package Admin;
 
-import java.io.*;
-
 public class EmployeeUser {
 
     private String employeeId;
@@ -25,43 +23,4 @@ public class EmployeeUser {
     public String getSearchKey() {
         return this.employeeId;
     }
-
-    public void saveToFile() {
-
-        File file = new File("Employees.txt");
-
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            if (isDuplicateId(file)) {
-                System.out.println("Error Employee Id " + employeeId + " already exists!");
-                return;
-            }
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-                writer.write(lineRepresentation());
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error writing to file:" + e.getMessage());
-        }
-    }      
-    
-
-    private boolean isDuplicateId(File file) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-
-            while ((line= reader.readLine()) != null) {
-                String[] data = line.split(",");
-                if (data.length > 0 && data[0].equals(employeeId)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 }

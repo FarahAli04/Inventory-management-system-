009774c324617ee8224c31b1e3966c6a6771260c
FarahAli04 <es.farah.ali2023@alexu.edu.eg>
2025-10-18 21:24:33 +0300

    update main

--- lab4/src/main/java/com/example/Admin/EmployeeUser.java
+++ lab4/src/main/java/com/example/Admin/EmployeeUser.java
@@ -9,11 +9,37 @@ public class EmployeeUser {
     private String phoneNumber;
 
     public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.email = email;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
+        if(employeeId == null || employeeId.trim().isEmpty()) {
+            throw new IllegalArgumentException("Employee ID cannot be null or empty");
+        }
+        else{
+            this.employeeId = employeeId;
+        }
+        if(name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        else{
+            this.name = name;
+        }
+        if(email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+        else{
+            this.email = email;
+        }
+        if (address == null || address.trim().isEmpty()) {
+             throw new IllegalArgumentException("Address cannot be null or empty");
+        }
+        else{
+            this.address = address;
+        }
+        if(phoneNumber == null || phoneNumber.trim().isEmpty()) {
+            throw new IllegalArgumentException("Phone number cannot be null or empty");
+        }
+        else{
+            this.phoneNumber = phoneNumber;
+        }
+        
     }
 
     public String lineRepresentation() {

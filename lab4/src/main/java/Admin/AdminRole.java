aa62e63889bd3c8ccb298c9e9dd09ce2d13422ab
Habiba-Aboghazala9 <es.habiba.salah2023@alexu.edu.eg>
2025-10-18 20:22:39 +0300

    3rd class

---
+++ lab4/src/main/java/Admin/AdminRole.java
@@ -0,0 +1,26 @@
+package Admin;
+import java.util.ArrayList;
+
+public class AdminRole {
+    private EmployeeUserDatabase database;
+    
+    public AdminRole(){
+        this.database = new EmployeeUserDatabase("Employees.txt");
+    }
+    
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber){
+        EmployeeUser newEmployee =new EmployeeUser(employeeId,name,email,address,phoneNumber);
+        database.insertRecord(newEmployee);
+    }
+    public EmployeeUser[] getListOfEmployees(){
+        ArrayList<EmployeeUser> l=database.returnAllRecords();
+        return l.toArray(new EmployeeUser[0]);
+    }
+    
+    public void removeEmployee(String key){
+        database.deleteRecord(key);
+    }
+    public void logout(){
+        database.saveToFile();
+    }
+}
